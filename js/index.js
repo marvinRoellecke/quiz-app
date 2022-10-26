55dc1a51a9c44929d7eba8947c2e3f674301b464
Marvin Roellecke <marvin@roellecke.com>
2022-10-26 12:55:58 +0200

    fix wrong css-name found by Alex B

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ const bodyElement = document.querySelector('[data-js="body"]');
 const toggleButton = document.querySelector('[data-js="button__dark-mode"]');
 
 toggleButton.addEventListener("click", () => {
-  bodyElement.classList.toggle("body__dark");
+  bodyElement.classList.toggle("body--dark");
   console.log("Clicked dark mode button");
 });
