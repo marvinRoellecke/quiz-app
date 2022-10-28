1a48175d88530cfe0482acefe5082cc6d4e085f1
Marvin Roellecke <marvin@roellecke.com>
2022-10-28 18:36:32 +0200

    make dark-mode available for all three .html

---
+++ js/bookmarks.js
@@ -0,0 +1,35 @@
+const answerB1 = document.querySelector('[data-js="p__answerB1"]');
+const btnAnswerB1 = document.querySelector('[data-js="btnAnswerB1"]');
+
+btnAnswerB1.addEventListener("click", () => {
+  answerB1.classList.toggle("show-answer");
+  console.log("Clicked answer button");
+});
+const answerB4 = document.querySelector('[data-js="p__answerB4"]');
+const btnAnswerB4 = document.querySelector('[data-js="btnAnswerB4"]');
+
+btnAnswerB4.addEventListener("click", () => {
+  answerB4.classList.toggle("show-answer");
+  console.log("Clicked answer button");
+});
+const answerB5 = document.querySelector('[data-js="p__answerB5"]');
+const btnAnswerB5 = document.querySelector('[data-js="btnAnswerB5"]');
+
+btnAnswerB5.addEventListener("click", () => {
+  answerB5.classList.toggle("show-answer");
+  console.log("Clicked answer button");
+});
+const answerB7 = document.querySelector('[data-js="p__answerB7"]');
+const btnAnswerB7 = document.querySelector('[data-js="btnAnswerB7"]');
+
+btnAnswerB7.addEventListener("click", () => {
+  answerB7.classList.toggle("show-answer");
+  console.log("Clicked answer button");
+});
+const answerB10 = document.querySelector('[data-js="p__answerB10"]');
+const btnAnswerB10 = document.querySelector('[data-js="btnAnswerB10"]');
+
+btnAnswerB10.addEventListener("click", () => {
+  answerB10.classList.toggle("show-answer");
+  console.log("Clicked answer button");
+});
--- js/profile.js
+++ js/profile.js
@@ -2,16 +2,64 @@
 
 const bodyElement = document.querySelector('[data-js="body"]');
 const toggleBtn = document.querySelector('[data-js="button__dark-mode"]');
+const quizCard1 = document.querySelector('[data-js="quizCard1"]');
+const quizCard2 = document.querySelector('[data-js="quizCard2"]');
+const quizCard3 = document.querySelector('[data-js="quizCard3"]');
+const quizCard4 = document.querySelector('[data-js="quizCard4"]');
+const quizCard5 = document.querySelector('[data-js="quizCard5"]');
+const quizCard6 = document.querySelector('[data-js="quizCard6"]');
+const quizCard7 = document.querySelector('[data-js="quizCard7"]');
+const quizCard8 = document.querySelector('[data-js="quizCard8"]');
+const quizCard9 = document.querySelector('[data-js="quizCard9"]');
+const quizCard10 = document.querySelector('[data-js="quizCard10"]');
+const quizCardB1 = document.querySelector('[data-js="quizCardB1"]');
+const quizCardB4 = document.querySelector('[data-js="quizCardB4"]');
+const quizCardB5 = document.querySelector('[data-js="quizCardB5"]');
+const quizCardB7 = document.querySelector('[data-js="quizCardB7"]');
+const quizCardB10 = document.querySelector('[data-js="quizCardB10"]');
+const profileCard = document.querySelector('[data-js="profileCard"]');
 let darkMode = localStorage.getItem("dark-mode");
 
 const enableDarkMode = () => {
   bodyElement.classList.add("body--dark");
+  quizCard1.classList.add("quiz-card--dark");
+  quizCard2.classList.add("quiz-card--dark");
+  quizCard3.classList.add("quiz-card--dark");
+  quizCard4.classList.add("quiz-card--dark");
+  quizCard5.classList.add("quiz-card--dark");
+  quizCard6.classList.add("quiz-card--dark");
+  quizCard7.classList.add("quiz-card--dark");
+  quizCard8.classList.add("quiz-card--dark");
+  quizCard9.classList.add("quiz-card--dark");
+  quizCard10.classList.add("quiz-card--dark");
+  quizCardB1.classList.add("quiz-card--dark");
+  quizCardB4.classList.add("quiz-card--dark");
+  quizCardB5.classList.add("quiz-card--dark");
+  quizCardB7.classList.add("quiz-card--dark");
+  quizCardB10.classList.add("quiz-card--dark");
+  profileCard.classList.add("quiz-card--dark");
   localStorage.setItem("dark-mode", "enabled");
   console.log("Clicked dark mode button");
 };
 
 const disableDarkMode = () => {
   bodyElement.classList.remove("body--dark");
+  quizCard1.classList.remove("quiz-card--dark");
+  quizCard2.classList.remove("quiz-card--dark");
+  quizCard3.classList.remove("quiz-card--dark");
+  quizCard4.classList.remove("quiz-card--dark");
+  quizCard5.classList.remove("quiz-card--dark");
+  quizCard6.classList.remove("quiz-card--dark");
+  quizCard7.classList.remove("quiz-card--dark");
+  quizCard8.classList.remove("quiz-card--dark");
+  quizCard9.classList.remove("quiz-card--dark");
+  quizCard10.classList.remove("quiz-card--dark");
+  quizCardB1.classList.remove("quiz-card--dark");
+  quizCardB4.classList.remove("quiz-card--dark");
+  quizCardB5.classList.remove("quiz-card--dark");
+  quizCardB7.classList.remove("quiz-card--dark");
+  quizCardB10.classList.remove("quiz-card--dark");
+  profileCard.classList.remove("quiz-card--dark");
   localStorage.setItem("dark-mode", "disabled");
   console.log("Clicked dark mode button");
 };
